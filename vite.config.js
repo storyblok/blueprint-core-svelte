b107c49dfd3c8978cfdf1d3e8659f1ab151249c4
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2025-07-02 17:18:09 +0530

    feat: updated env file to match with other blueprints

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
-export default defineConfig({
-	plugins: [sveltekit(), mkcert()],
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [sveltekit(), mkcert()],
+		define: {
+			'import.meta.env.STORYBLOK_DELIVERY_API_TOKEN': JSON.stringify(
+				env.STORYBLOK_DELIVERY_API_TOKEN,
+			),
+			'import.meta.env.STORYBLOK_API_BASE_URL': JSON.stringify(
+				env.STORYBLOK_API_BASE_URL,
+			),
+		},
+	};
 });
